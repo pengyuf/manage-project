b82141293bb6f41b5f417d900b19f2e0cc41add3
best <2907647445@qq.com>
2023-01-15 15:12:06 +0800

    初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>首页</div>
+</template>
+
+<script lang="ts" setup>
+import { ref,reactive, toRefs, onMounted, getCurrentInstance,computed } from 'vue'
+
+</script>
+<style scoped>
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, reactive, toRefs, onMounted, getCurrentInstance, computed } from 'vue'
+
+</script>
+<style scoped lang="less">
+
+</style>
